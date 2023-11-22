71ca65f2bad145f2eb70b1c74015909eb8fb4d2d
KasparTruu <88343009+KasparTruu@users.noreply.github.com>
2023-11-22 09:45:29 +0200

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,11 @@
+const headPhones = ["Headphone1", "Headphone2", "Headphone3", "Headphone4"];
+
+for (let index = 0; index < headPhones.length; index++) {
+    console.log(headPhones[index]);
+}
+
+//  or – tsüklid läbi koodiploki mitu korda
+// for/in - tsüklid läbi objekti omaduste
+// for/of - tsüklid läbi itereeritava objekti väärtuste
+// while – läbib koodiploki, kui määratud tingimus on tõene
+//  do/while – läbib ka koodiploki, kui määratud tingimus on tõene
