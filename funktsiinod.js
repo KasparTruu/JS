50fea2d18c5ca882312cccc245ae5ddca3ffe6b2
KasparTruu <88343009+KasparTruu@users.noreply.github.com>
2023-11-22 09:45:56 +0200

    Create funktsiinod.js

---
+++ funktsiinod.js
@@ -0,0 +1,8 @@
+// Funktsioon kutsutakse välja, tagastatav väärtus on x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Funktsioon tagastab a ja b korrutise
+   return a * b;
+}
+// Ehk ta tagastab selle väärtuse sulle milleks in 4 ja 3
